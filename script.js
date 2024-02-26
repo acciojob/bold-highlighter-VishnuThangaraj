e32eb3657995373ec58f1d590a0d1abacdfad329
Vishnu Thangaraj <110882983+VishnuThangaraj@users.noreply.github.com>
2024-02-26 19:28:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
+const strongTags = [...document.getElementsByTagName ('strong')];
+
 function highlight() {
-    //Write your code here
-
-
+	strongTags.forEach(function(element) {
+            element.classList.add('green');
+        });
 }
 
 
 function return_normal() {
     //Write your code here
-
+strongTags.forEach(function(element) {
+            element.classList.remove('green');
+        });
     
 }
